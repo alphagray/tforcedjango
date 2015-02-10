5833c8268c5f952df9b7c5dc592f7ca961d0c0f3
alphagray <srboyce@outlook.com>
2015-02-10 17:14:31 -0500

    began diversifying models and application.

--- tforce/models.py
+++ tforce/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+
+    STATUS_CHOICES = (
+        (1, _('Draft')),
+        (2, _('Publics')),
+    )
+
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    status = models.IntegerField(_('status'), choices=STATUS_CHOICES, 
+                                 default=2)
+
+class ExtendedPost(models.Model, Post):
+    pass
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+
+    def __unicode__(self):
+        return self.question
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length = 200)
+    votes = models.IntegerField(default = 0)
+
+    def __unicode__(self):
+        return self.choice_text

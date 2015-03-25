0fd6dd611fbcfc0cae48d6a73ccce9f601d292c0
alphagray <srboyce@outlook.com>
2015-03-25 18:09:34 -0400

    fixed the settings, put them back.

--- DjangoWebProject/settings.py
+++ DjangoWebProject/settings.py
@@ -76,7 +76,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = 'D:/home/site/wwwroot/static'
+STATIC_ROOT = path.join(PROJECT_ROOT, 'static').replace('\\', '/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"

b8e1cc4c055647a22a26579a9667ca659b1494fa
alphagray <srboyce@outlook.com>
2015-03-26 12:13:19 -0400

    static files fix

--- DjangoWebProject/settings.py
+++ DjangoWebProject/settings.py
@@ -7,8 +7,8 @@ PROJECT_ROOT = path.dirname(path.abspath(path.dirname(__file__)))
 
 ROOT_PATH = path.join(path.dirname(__file__), '..')
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+#DEBUG = True
+#TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = (
     'localhost',
@@ -78,7 +78,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = path.join(PROJECT_ROOT, 'static').replace('\\', '/')
+STATIC_ROOT = path.join(ROOT_PATH, 'static').replace('\\', '/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -86,7 +86,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    path.abspath(path.join(STATIC_ROOT, STATIC_URL)),
+    #path.abspath(path.join(ROOT_PATH, "static")),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
